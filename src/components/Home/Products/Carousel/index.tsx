d4df1979f994e3dbf7b84f62c83fc72806a57e2a
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2021-06-11 16:57:32 -0300

    swipe catalogs

--- src/components/Home/Products/Carousel/index.tsx
+++ src/components/Home/Products/Carousel/index.tsx
@@ -1,7 +1,10 @@
 import {carouselItems} from './carouselItems'
 import {CarouselContainer} from './styles'
 
-import Swiper from 'react-id-swiper';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 
 
@@ -9,32 +12,18 @@ import Swiper from 'react-id-swiper';
 
 export function CarouselProducts() {
 
-
-  const params = {
-    slidesPerView: 1.50,
-    spaceBetween: 30,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true
-    },
-    autoplay: {
-      delay: 1500,
-      disableOnInteraction: false
-    }
-  }
-
     
     return(
           <CarouselContainer>
   
-          <Swiper {...params}>
-              {carouselItems.map((item, index) =>{
+  <AutoPlaySwipeableViews>
+  {carouselItems.map((item, index) =>{
                 return (
                        <img key={index} src={item.img} alt="" />
                 )
                
             })}
-        </Swiper>
+  </AutoPlaySwipeableViews>
   
         </CarouselContainer>
     
