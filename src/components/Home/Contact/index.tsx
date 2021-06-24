3de117c86ea5c086fd020da4271292db02a680b2
rodrigobergamindev <rodrigobergamindev@gmail.com>
2021-06-23 22:18:59 -0300

    contact

---
+++ src/components/Home/Contact/index.tsx
@@ -0,0 +1,91 @@
+import {Container} from './styles'
+
+
+
+export function Contact() {
+
+
+
+    return (
+        <Container>
+
+            <div className="header">
+                <div className="title">
+                    <h2>Encontre-nos nas redes sociais</h2>
+                </div>
+
+                
+                    <ul>
+                        <li>
+                            <div className="telegram">
+                                <img src="/img/socialMedia/contato.png" alt="" />
+
+                                <div className="subtitle">
+                                <p>Lojinha da Mayumi</p>
+                                <p>Telegram</p>
+                                </div>
+                                
+                            </div>
+                        </li>
+
+                        <li>
+
+                        <div className="whatsapp">
+                                <img src="/img/icons/whatsapp.png" alt="" />
+
+                                <div className="subtitle">
+                                <p>(11) 2963-9226</p>
+                                <p>WhatsApp</p>
+                                </div>
+                                
+                            </div>
+
+                        </li>
+                        <li>
+
+                        <div className="facebook">
+                                <img src="/img/icons/facebook.png" alt="" />
+
+                                <div className="subtitle">
+                                <p>@lojinhadamayumi</p>
+                                <p>Facebook</p>
+                                </div>
+                                
+                            </div>
+
+                        </li>
+
+                        <li>
+
+                        <div className="instagram">
+                                <img src="/img/socialMedia/instagramWhite.png" alt="" />
+
+                                <div className="subtitle">
+                                <p>@lojinha_da_mayumi</p>
+                                <p>Instagram</p>
+                                </div>
+                                
+                            </div>
+
+                        </li>
+
+                        <li>
+
+                        <div className="tiktok">
+                                <img src="/img/icons/tiktok.png" alt="" />
+
+                                <div className="subtitle">
+                                <p>@lojinhadamayumi</p>
+                                <p>Tik Tok</p>
+                                </div>
+
+                            </div>
+
+                        </li>
+                    </ul>
+                </div>
+           
+        </Container>
+    )
+}
+
