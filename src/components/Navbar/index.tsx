bdaa4cabf37532a47d162c6ae24a5eb205016689
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2021-06-24 13:45:26 -0300

    dropdown menu

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,7 +1,6 @@
-import {Container, Nav, ContainerNav, DropdownMenu} from './styles'
+import {Container, Nav, ContainerNav, DropdownMenu, SubMenu, ContainerSubMenu, SubContainer} from './styles'
 import {Logo} from '../Logo/index'
 import {Contato} from './Contato'
-import {Submenu} from './Submenu/Submenu'
 import {useState} from 'react'
 import {menu} from './assets/nav'
 
@@ -21,7 +20,10 @@ export function Navbar() {
         {menu.map((item, index) => {
           if(item.title === 'Produtos') {
             return (
-              <DropdownMenu active={active} key={index} onClick={() => setActive(!active)}><span>{item.title}</span><img className="arrow" src="/img/icons/arrowRed.png" alt="submenu"></img>
+              <DropdownMenu active={active}
+              onClick={() => setActive(!active)}
+              key={index} 
+              ><span>{item.title}</span><img className="arrow" src="/img/icons/arrowRed.png" alt="submenu"></img>
               </DropdownMenu>
             )
           }
@@ -33,10 +35,73 @@ export function Navbar() {
 
       <Contato/>
       </Nav>
-      <Submenu active={active} onMouseOver={() => {
-        setActive(false)
-        console.log(active)
-      }}/>
+
+      <SubMenu active={active} onMouseLeave={() => setActive(false)}>
+      <SubContainer>
+
+      
+      <ContainerSubMenu>
+        <h4>BERLOQUES</h4>
+      <ul>
+        <li>Banhados a Prata</li>
+        <li>Envernizados</li>
+        <li>Níquel Free</li>
+      </ul>
+      </ContainerSubMenu>
+
+      <ContainerSubMenu>
+        <h4>PULSEIRAS</h4>
+      <ul>
+        <li>Banhadas a Prata</li>
+        <li>Envernizadas</li>
+        <li>Braceletes</li>
+      </ul>
+      </ContainerSubMenu>
+
+      <ContainerSubMenu>
+        <h4>TRAVAS</h4>
+      <ul>
+        <li>Clip Strass</li>
+        <li>Clip Comum</li>
+        <li>Corrente Comum</li>
+        <li>Corrente Coração</li>
+        <li>Clip Corrente Coração</li>
+        <li>Clip Banhada</li>
+        <li>Corrente Banhada</li>
+        <li>Corrente Envernizada</li>
+        <li>Silicone</li>
+      </ul>
+      </ContainerSubMenu>
+
+      <ContainerSubMenu>
+        <h4>MURANOS</h4>
+      <ul>
+        <li>Murano</li>
+
+      </ul>
+      </ContainerSubMenu>
+
+      <ContainerSubMenu>
+        <h4>OUTROS</h4>
+      <ul>
+        <li>Colar com Pingente</li>
+        <li>Colar Tiffany</li>
+        <li>Pulseira Tiffany</li>
+
+      </ul>
+      </ContainerSubMenu>
+
+      <ContainerSubMenu>
+        <h4>ARGOLAS</h4>
+      <ul>
+        <li>Folheadas</li>
+        <li>Cravejadas</li>
+
+      </ul>
+      </ContainerSubMenu>
+      </SubContainer>
+
+      </SubMenu>
     </Container>
   )
 }
--- src/components/Navbar/styles.ts
+++ src/components/Navbar/styles.ts
@@ -95,3 +95,62 @@ export const ButtonContato = styled.button`
 
 
 `
+
+
+
+export const SubMenu = styled.nav`
+
+ 
+  display: flex;
+  width: 100%;
+
+  justify-content: center;
+  height:${props => props.active ? "24rem" : 0};
+  overflow: hidden;
+  transition: height 0.5s;
+`;
+
+export const SubContainer = styled.div`
+
+
+  display: flex;
+  width: 100%;
+  max-width: 1120px;
+  padding: 2rem;
+  justify-content:space-between;
+`;
+
+
+
+export const ContainerSubMenu = styled.div`
+
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+
+    img {
+        max-width:70px;
+        border-radius: 100%;
+    }
+ 
+ h4 {
+     margin-bottom: 0.5rem;
+     color: var(--primary);
+
+ }
+ul {
+    
+    li {
+        line-height: 2rem;
+        font-size: 0.9rem;
+        transition: 0.3s ease-in-out;
+        cursor: pointer;
+    &:hover {
+        color: var(--primary);
+        transition: 0.3s ease-in-out;
+    }
+    }
+    list-style: none;
+}
+`;
+
