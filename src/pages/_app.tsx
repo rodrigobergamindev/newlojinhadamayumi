2ab892305acfd288378c2ae2bfa88c7f317719df
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2021-07-15 13:25:31 -0300

    add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,22 @@
 import '../styles/globals.scss'
 
-function MyApp({ Component, pageProps }) {
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
+
+const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default App
