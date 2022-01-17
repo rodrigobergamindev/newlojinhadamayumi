9b19de97c1a2b8bda3e35d91f218bef832a1a609
rodrigobergamindev <rodrigobergamindev@gmail.com>
2022-01-17 09:35:58 -0300

    .

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,19 +1,22 @@
 
-import {Navbar} from '../components/Navbar/index'
+
 import Head from 'next/head'
 import {GlobalStyle} from  '../global'
 import { Home } from '../components/Home'
-import {useState} from 'react'
-import {Container, StyledBurger, Nav} from '../components/NavbarMobile/styles'
-import {carouselItems} from '../components/Home/Products/Carousel/carouselItems'
-import Link from 'next/link'
+import {useState, useEffect} from 'react'
+import {Container, StyledBurger} from '../components/NavbarMobile/styles'
 import { Logo } from '../components/Logo'
+import { useRouter } from 'next/router'
 
 
 
 export default function App() {
 
   const [open, setOpen] = useState(false)
+  const router = useRouter()
+  useEffect(() => {
+    router.push('https://www.lojinhadamayumi.com.br/')
+  });
 
   return (
     <>
@@ -21,33 +24,7 @@ export default function App() {
             <title>Lojinha da Mayumi</title>
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             </Head>
-      
-            <Container>
-        <Logo/>
-        <StyledBurger open={open} onClick={() => setOpen(!open)}>
-        <div />
-        <div />
-        <div />
-        </StyledBurger>
-      </Container>
 
-      <Nav open={open}>
-
-        <h2>MENU</h2>
-
-        <div className="menu">
-          {carouselItems.map((item, index) => {
-                  return (
-                      <a key={index} href={item.url} type="application/pdf" download><span>{item.title}</span></a>
-                  )
-              })}
-
-        </div>
-        </Nav>
-      
-      <Home open={open}/>
-      
-      <a href="https://api.whatsapp.com/send?phone=551129639226&text=Ol%C3%A1%2C%20gostaria%20de%20fazer%20meu%20pedido!" target="_blank"><img src="/img/socialMedia/whatsappfloat.png" className="iconFloat"/></a>
 
 
       <GlobalStyle/>
